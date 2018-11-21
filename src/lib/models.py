915729b2d6fc46bed08ca3581bffa466b81c0929
Carlos M. Cabrera <carlos@whitespaceventures.com>
2018-11-21 04:15:57 -0300

    volatile because we don't care about loosing some hits

--- src/lib/models.py
+++ src/lib/models.py
@@ -8,9 +8,9 @@ db = Database()
 class Node(db.Entity):
     id = PrimaryKey(int, auto=True)
     url = Required(str, unique=True)
-    strengh = Optional(Decimal, default=0)
+    strengh = Optional(Decimal, default=0, volatile=True)
     runs = Optional(int, default=0, unsigned=True)
-    m8 = Optional(Decimal, default=0)
-    m16 = Optional(Decimal, default=0)
-    m32 = Optional(Decimal, default=0)
+    m8 = Optional(Decimal, default=0, volatile=True)
+    m16 = Optional(Decimal, default=0, volatile=True)
+    m32 = Optional(Decimal, default=0, volatile=True)
 
