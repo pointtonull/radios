e43cafe8335d147da23ec6c0f1f70c13b6ba9ca7
Carlos M. Cabrera <carlos@whitespaceventures.com>
2018-12-07 10:10:38 -0300

    make them volatile to allow crossed changes

--- src/lib/models.py
+++ src/lib/models.py
@@ -9,8 +9,8 @@ class Node(db.Entity):
     id = PrimaryKey(int, auto=True)
     url = Required(str, unique=True)
     strengh = Optional(Decimal, default=0, volatile=True)
-    runs = Optional(int, default=0, unsigned=True)
-    m8 = Optional(Decimal, default=0, volatile=True)
-    m16 = Optional(Decimal, default=0, volatile=True)
-    m32 = Optional(Decimal, default=0, volatile=True)
+    runs = Optional(int, default=0, volatile=True, unsigned=True)
+    m12 = Optional(Decimal, default=0, volatile=True)
+    m24 = Optional(Decimal, default=0, volatile=True)
+    cache = Optional('Cache')
 
