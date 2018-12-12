221cd08284066aa0238b6c8a18edeff96c44adc3
Carlos M. Cabrera <carlos@whitespaceventures.com>
2018-12-12 15:53:43 -0300

    cache is independet of nodes now

--- src/lib/models.py
+++ src/lib/models.py
@@ -8,15 +8,14 @@ db = Database()
 class Node(db.Entity):
     id = PrimaryKey(int, auto=True)
     url = Required(str, unique=True)
-    strengh = Optional(Decimal, default=0, volatile=True)
-    runs = Optional(int, default=0, volatile=True, unsigned=True)
-    m12 = Optional(Decimal, default=0, volatile=True)
-    m24 = Optional(Decimal, default=0, volatile=True)
-    cache = Optional('Cache')
+    strengh = Optional(Decimal, default=200, volatile=True)
+    runs = Optional(int, default=1, volatile=True, unsigned=True)
+    m8 = Optional(Decimal, default=200, volatile=True)
+    m16 = Optional(Decimal, default=200, volatile=True)
 
 
 class Cache(db.Entity):
     id = PrimaryKey(int, auto=True)
-    lastupdated = Required(float)
-    content = Optional(LongStr, lazy=True)
-    node = Required(Node)
+    url = Required(str, unique=True)
+    lastupdated = Optional(float, default=0)
+    content = Optional(LongStr)
